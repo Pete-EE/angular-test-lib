5c9350875e364d528a272eed1776eb8b68e68dfa
Pete <phallock@emersonecologics.com>
2019-12-03 16:06:09 -0500

    initial commit

---
+++ new-workspace/projects/angular-test-lib/src/lib/angular-test-lib.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lib-angular-test-lib',
+  template: `
+    <p>
+      angular-test-lib works!
+    </p>
+  `,
+  styles: []
+})
+export class AngularTestLibComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
